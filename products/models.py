3be92152e643fee1cf125e6c4cacbcabddd8b74f
E.T Moronfolu <emmanuelmoronfolu6@gmail.com>
2025-03-04 02:10:35 +0100

    added product database/models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
+
+User=get_user_model()
 # Create your models here.
+
+class Product(models.Model):
+    owner=models.ForeignKey(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=200)
+    description=models.TextField()
+    price=models.DecimalField(max_digits=10,decimal_places=2)
+    stock=models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.name
