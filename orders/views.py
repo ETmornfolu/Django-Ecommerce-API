d7cf470c465721634341fd6a0d83c98fb46e1b66
E.T Moronfolu <emmanuelmoronfolu6@gmail.com>
2025-03-10 15:06:52 +0100

    added the order and cart views and urls

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from core.permissions import IsSeller,IsBuyer,IsAdmin
+from .models import Order,Cart
+from .serializers import OrderSerializer,CartSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class CartViewSet(viewsets.ModelViewSet):
+    queryset=Cart.objects.all()
+    permission_classes=[IsAuthenticated,IsBuyer]
+    serializer_class=CartSerializer
+    
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset=Order.objects.all()
+    permission_classes=[IsAuthenticated,IsBuyer]
+    serializer_class=OrderSerializer
+    
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+    
