4f523ba7a32537eb1f6ef20e39d8d8b8b2caa9d6
E.T Moronfolu <emmanuelmoronfolu6@gmail.com>
2025-04-13 01:12:54 +0100

    change sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-echo "Applying database migrations..."
+echo "Running migrations..."
 python manage.py migrate --noinput
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting server..."
-gunicorn ecommerce.wsgi:application --bind 0.0.0.0:8000
+gunicorn ecommerce.wsgi:application --bind 0.0.0.0:8000 --workers 1 --threads 2 --timeout 60
+
