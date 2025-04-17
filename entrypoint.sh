44ad3aa138835bd3eb4a899af8ae2bd784ce0e24
E.T Moronfolu <emmanuelmoronfolu6@gmail.com>
2025-04-17 11:41:12 +0100

    edited entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -e  # Exit on any error
 echo "🚀 Running Django migrations..."
 python manage.py migrate --noinput
 
-echo "📦 Collecting static files..."
-python manage.py collectstatic --noinput
+# echo "📦 Collecting static files..."
+# python manage.py collectstatic --noinput
 
 echo "✅ Entrypoint tasks complete. Starting server..."
 exec "$@"
